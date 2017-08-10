e79e608baecd270329da22c0bdd3618ee6a3c6f6
Michael Angerman <stormasm@gmail.com>
2017-08-10 14:45:11 -0700

    pp

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,48 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { checkout } from '../actions'
-import { getTotal, getCartProducts } from '../reducers'
-import Cart from '../components/Cart'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { checkout } from "../actions";
+import { getTotal, getCartProducts } from "../reducers";
+import Cart from "../components/Cart";
 
-import ProductItem from '../components/ProductItem'
-import ProductsList from '../components/ProductsList'
+import ProductItem from "../components/ProductItem";
+import ProductsList from "../components/ProductsList";
 
-const CartContainer = ({ products, total, checkout }) => (
-
-    <div>
+const CartContainer = ({ products, total, checkout }) =>
+  <div>
     <ProductsList title="Products">
       {products.map(product =>
         <ProductItem
           key={product.id}
           product={product}
-          onAddToCartClicked={() => addToCart(product.id)} />
+          onAddToCartClicked={() => addToCart(product.id)}
+        />
       )}
     </ProductsList>
-  <Cart
-    products={products}
-    total={total}
-    onCheckoutClicked={() => checkout(products)} />
-    </div>
-)
+    <Cart
+      products={products}
+      total={total}
+      onCheckoutClicked={() => checkout(products)}
+    />
+  </div>;
 
 CartContainer.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    quantity: PropTypes.number.isRequired
-  })).isRequired,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      quantity: PropTypes.number.isRequired
+    })
+  ).isRequired,
   total: PropTypes.string,
   checkout: PropTypes.func.isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   products: getCartProducts(state),
   total: getTotal(state)
-})
+});
 
-export default connect(
-  mapStateToProps,
-  { checkout }
-)(CartContainer)
+export default connect(mapStateToProps, { checkout })(CartContainer);
